6d73e2bfc2f9ebab738194bc8541432bbaea1067
Damian Wantzing <damian@MacBook-Air-van-Damian.local>
2024-08-23 20:57:23 +0200

    Renamed execution to methods

--- jsonrpc.go
+++ jsonrpc.go
@@ -1,1 +1,13 @@
 package jsonrpc
+
+import "io"
+
+type JsonRPC struct {
+}
+
+// HandleRequest handles an incoming JSON-RPC request
+// and returns the response from the called method
+// via the provided writer
+func (j *JsonRPC) HandleRequest(writer io.Writer, json []byte) {
+
+}
--- method.go
+++ method.go
@@ -1,21 +1,21 @@
 package jsonrpc
 
-// A single parameter for an Executable method
+// A single parameter for a Function
 type parameter struct {
 	Name string
 	Type string
 }
 
-// This is a wrapper around an executable method
+// This is a wrapper around a Function
 // that is exposed to the JSONRPC protocol
-type Executable struct {
+type Function struct {
 	name   string
 	exec   func(params map[string]interface{}) (interface{}, error)
 	params []parameter
 }
 
-// Execute an executable method
-func (e *Executable) execute(request Request) (interface{}, error) {
+// Execute a Function
+func (m *Function) execute(request Request) (interface{}, error) {
 	// TODO: we probably cannot pass the params as is
 	// since they can be passed in two ways: by name or by position
 	// return e.exec(request.Params)
@@ -23,34 +23,34 @@ func (e *Executable) execute(request Request) (interface{}, error) {
 	return nil, nil
 }
 
-// The Executor hold the methods that will be executed
+// The Methods struct hold the methods that will be executed
 // when specifically requested by the JSONRPC protocol
 // if you want to expose a new method to JSONRPC, this
 // is where you add it
-type Executor struct {
-	methods map[string]Executable
+type Methods struct {
+	functions map[string]Function
 }
 
-// Creates and returns a new Executor
-func New() Executor {
-	return Executor{
-		methods: make(map[string]Executable),
+// Creates and returns a new Methods
+func New() Methods {
+	return Methods{
+		functions: make(map[string]Function),
 	}
 }
 
-// Add a method to the Executor
+// Add a method to the Methods
 // This will expose the method to the JSONRPC protocol
-func (e *Executor) Add(method Executable) {
-	e.methods[method.name] = method
+func (e *Methods) Add(function Function) {
+	e.functions[function.name] = function
 }
 
 // Remove a method from the executor
 // This will hide the method from the JSONRPC protocol
-func (e *Executor) Remove(name string) {
-	delete(e.methods, name)
+func (e *Methods) Remove(name string) {
+	delete(e.functions, name)
 }
 
 // Execute a method from a JSONRPC message
-func (e *Executor) execute(request Request) (*Response, *Error) {
+func (e *Methods) execute(request Request) (*Response, *Error) {
 	return nil, nil
 }
